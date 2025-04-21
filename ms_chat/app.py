20f7a80e4c7317a8aba18e99c6007fa95044cda0
Leonardo <leonardojpsedasilva@gmail.com>
2025-04-21 18:59:07 +0000

    MVP chat finish

---
+++ ms_chat/app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import asyncio
+from client_server import MCPClient
+
+with st.sidebar:
+    st.title("MCP Chatbot with Claude LLM")    
+    server_url = st.text_input("Enter the server URL:")
+    button = st.button("Connect to Server MCP and start chat")
+    if button:
+        st.session_state.server_url = server_url
+        st.success("Connected to server!")
+
+async def ask_bot(server_url, prompt):
+    client = MCPClient()
+    await client.connect_to_server(server_url)
+    response = await client.process_query(prompt)
+    await client.close()
+    return response
+
+# Initialize history of messages in session
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# display chat history e chat input
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+history = ""
+for msg in st.session_state.messages:
+    history += f"{msg['role']}: {msg['content']}\n"
+
+prompt = st.chat_input("Fale com o sistema de informacão de Clientes")
+if server_url and prompt:
+    st.chat_message("user").write(prompt)
+    user = st.session_state.messages.append({"role": "user", "content": prompt})
+
+    full_prompt = history + f"user: {prompt}\n"
+    response = asyncio.run(ask_bot(server_url, full_prompt))
+    st.chat_message("assistant").write(response)
+    assistant = st.session_state.messages.append({"role": "assistant", "content": response})
---
+++ ms_chat/client_server.py
@@ -0,0 +1,148 @@
+import asyncio
+import sys
+from typing import Optional
+from contextlib import AsyncExitStack
+
+from mcp import ClientSession
+from mcp.client.sse import sse_client
+
+from anthropic import Anthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class MCPClient:
+    def __init__(self):
+        # Initialize the MCP client
+        self.session: Optional[ClientSession] = None
+        self.exit_stack = AsyncExitStack()
+        self.anthropic = Anthropic()
+
+    async def connect_to_server(self, server_url: str):
+        """
+        Connect to the MCP server.
+        """
+        # Connect to the MCP server
+        sse_transport = await self.exit_stack.enter_async_context(sse_client(server_url))
+        self.sse, self.write = sse_transport
+        self.session = await self.exit_stack.enter_async_context(ClientSession(self.sse, self.write))
+
+        await self.session.initialize()
+
+        #list available tools
+        response = await self.session.list_tools()
+        tools = response.tools
+        print(f"Available tools: {tools}")
+
+        print(f"Connected to MCP server at {server_url}")
+        
+    async def close(self):
+        """
+        Close the connection and release resources.
+        """
+        if self.exit_stack:
+            await self.exit_stack.aclose()
+            print("Connection closed and resources released.")
+
+    async def process_query(self, query: str):
+        """
+        Process a query using Claude and available tools.
+        """
+        messages = [
+            {
+                "role": "user",
+                "content": query,
+            }
+        ]
+
+        response = await self.session.list_tools()
+        available_tools = [{
+            "name": tool.name,
+            "description": tool.description,
+            "input_schema": tool.inputSchema
+        } for tool in response.tools]
+
+        # initialize the Anthropic client
+        response = self.anthropic.messages.create(
+            model="claude-3-5-sonnet-20241022",
+            max_tokens=1000,
+            messages=messages,
+            tools=available_tools
+        )
+
+        #process response and handle tools calls
+        final_text = []
+        
+        for content in response.content:
+            if content.type == "text":
+                final_text.append(content.text)
+            elif content.type == "tool_use":
+                tool_name = content.name
+                tools_args = content.input
+
+                # execute the tool
+                result = await self.session.call_tool(tool_name, tools_args)
+                final_text.append(f"[calling tool {tool_name} with args {tools_args}]")
+
+                # continue the conversation with the tool's result
+                if hasattr(content, "text") and content.text:
+                    messages.append({
+                            "role": "assistant",
+                            "content": content.text,
+                        })
+                messages.append({
+                    "role": "user",
+                    "content": result.content,
+                    })
+
+                # get next response from claude
+                response = self.anthropic.messages.create(
+                    model="claude-3-5-sonnet-20241022",
+                    max_tokens=1000,
+                    messages=messages
+                )
+
+                # response to user
+                final_text.append(response.content[0].text)
+
+        return "\n".join(final_text)
+
+    async def chat_loop(self, query: str):
+        """
+        Start a chat loop to interact with the user.
+        """
+        print("\nMCP Client Started!")
+        print("Type 'exit' to quit the chat.")
+
+        while True:
+            try:
+                # query = input("You: ").strip()
+
+                if query.lower() == "exit":
+                    print("Exiting chat...")
+                    break
+
+                response = await self.process_query(query)
+                print("\n" + response)
+            
+            except Exception as e:
+                print(f"An error occurred: {e}")
+            
+    async def cleanup(self):
+        """
+        Cleanup resources and close the connection.
+        """
+        await self.exit_stack.aclose()
+
+async def main(server_url_mcp):
+
+    client = MCPClient()
+    try:
+        await client.connect_to_server(server_url_mcp)
+        await client.chat_loop()
+    finally:
+        await client.cleanup()
+
+if __name__ == "__main__":
+    import sys
+    asyncio.run(main())
